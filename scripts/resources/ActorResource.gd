1cd1ad5d6a896064d37d526ab248d35d8637d5f6
DavidC320 <d.j.cruz230@gmail.com>
2024-04-12 11:13:36 -0400

    actor 2

--- scripts/resources/ActorResource.gd
+++ scripts/resources/ActorResource.gd
@@ -3,4 +3,10 @@ class_name ActorResource
 
 @export_category("Information")
 @export var name: String
+@export_category("Statistics")
+@export var health := 100
+@export var speed := 10
+@export var jump_height := -600
+@export var acenstion_time := .5
+@export var decension_time := .5
 
