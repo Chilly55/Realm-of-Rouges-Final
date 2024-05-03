c17f39e94d07541fbfe095bcae53fa9797f38ae8
Parris Oliver <poliver14@ivytech.edu>
2024-05-03 17:55:59 -0400

    Added Npc and dialogue box

--- NPC.gd
+++ NPC.gd
@@ -7,7 +7,7 @@ enum {
 	DIALOGUE
 }
 
-const speed = 30 
+const speed = 0 
 var current_state = IDLE 
 var dir = Vector2.RIGHT
 
@@ -15,8 +15,18 @@ func _process(delta):
 	match current_state:
 		IDLE:
 			pass
-		NEW_DIR:
-			dir = choose([Vector2.RIGHT, Vector2.UP, Vector2, LEFT, Vector2.DOWN])
+		
 		MOVE:
 			move(delta) 
+
+func move(delta):
+	position =+ dir * speed * delta 
+	
+func choose(array):
+	array.shuffle() 
+	return array.front() 
+	
+func _on_Timer_timeout():
+	$Timer.wait_time = choose([0.5, 1, 1.5])
+	current_state = choose(IDLE) 
 
